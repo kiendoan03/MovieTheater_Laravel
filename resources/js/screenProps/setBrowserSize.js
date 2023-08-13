34fc2f3f247861103680cfec267b4a19800767e5
Ngd_anh <dinhanh.2303@gmail.com>
2023-08-13 19:03:51 +0700

    create dev enviroment

---
+++ resources/js/screenProps/setBrowserSize.js
@@ -0,0 +1,13 @@
+var win = window,
+    doc = document,
+    docElem = doc.documentElement,
+    // body = doc.getElementsByTagName('body')[0],
+    x = win.innerWidth || docElem.clientWidth,
+    y = win.innerHeight|| docElem.clientHeight;
+
+document.querySelector('.full-screen-element').style.height = y + 'px';
+document.querySelector('.fullscreen-height').style.height = y + 'px';
+// document.querySelector('.full-screen-element').style.width = x + 'px';    
+// alert(x + ' × ' + y);
+
+document.body.style.overflow = "hidden";
