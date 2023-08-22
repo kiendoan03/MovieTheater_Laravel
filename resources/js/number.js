556a7fffab5c3912399b3cdd35fe9a6b9b0a218b
Kien Doan <kiendoan444@gmail.com>
2023-08-22 17:13:34 +0700

    top_movie

---
+++ resources/js/number.js
@@ -0,0 +1,8 @@
+let box = document.getElementById('box');
+let width = box.offsetWidth;
+let height = box.offsetHeight;
+
+let number = document.getElementById('number');
+number.style.height = height + "px";
+
+// alert('height =' + height + "width" + width);
