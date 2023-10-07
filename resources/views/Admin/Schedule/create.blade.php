7e0d0b61b086e7d8903467f6dada16cf1d62752f
Ngd_anh <dinhanh.2303@gmail.com>
2023-10-07 14:03:43 +0700

    a

--- resources/views/Admin/Schedule/create.blade.php
+++ resources/views/Admin/Schedule/create.blade.php
@@ -71,7 +71,7 @@
 
                         <!-- Main -->
                         <div class="row">
-                            <form role="form" method="post" action="{{route('admin.schedules.store')}}" enctype="multipart/form-data">
+                            <form role="form" method="post" action="{{route('admin.schedules.store')}}">
                                  @csrf
                                 <div class="row">
                                     <div class="col-8">
@@ -126,11 +126,6 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     <script>
         CKEDITOR.replace(movie_description);
-    </script>
-    <script>
-        function show_img() {
-            director_img.src = URL.createObjectURL(event.target.files[0]);
-        }
     </script>
 </body>
 
